96521d15515c9d0f228a51a96975d85ccf82459b
OlegKuzmenkov <nrg-2009@mail.ru>
2018-03-29 18:29:03 +0300

    create project

---
+++ app/src/main/java/com/oleg_kuzmenkov/android/nrglab3git/MainActivity.java
@@ -0,0 +1,16 @@
+package com.oleg_kuzmenkov.android.nrglab3git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
